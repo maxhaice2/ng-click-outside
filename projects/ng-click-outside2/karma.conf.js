3f86b60c4c8cc4652621e97464d82f7ba2d60c90
Kr0san89 <ManuelSchmidt23@gmail.com>
2023-04-28 23:29:07 +0200

    chore: CI Pipeline (#17)
    * chore: CI Pipeline
    
    * chore: update eslint (#18)
    
    Co-authored-by: Manuel Schmidt <ms@manuel-schmidt.at>
    
    * chore: update dependencies
    
    * chore: update karam config for ci
    
    * chore: comment out tests for now
    
    * chore: update build script & add script for deployment
    
    ---------
    
    Co-authored-by: Manuel Schmidt <ms@manuel-schmidt.at>

--- projects/ng-click-outside2/karma.conf.js
+++ projects/ng-click-outside2/karma.conf.js
@@ -36,9 +36,8 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
+    singleRun: true,
     restartOnFileChange: true
   });
 };
