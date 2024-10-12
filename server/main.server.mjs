40e91988ceb60be2dcd56e364b6e84b46bdfbd38
Kr0san89 <ManuelSchmidt23@gmail.com>
2024-10-12 17:24:14 +0000

    Deploying to gh-pages from @ Kr0san89/ng-click-outside@88846c0bf8d2a66b4dde83db78199e380ecc5234 🚀

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a,b}from"./chunk-IVYRPMW5.mjs";import"./chunk-JV2FTZJZ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as app,b as default};
+import{a,b}from"./chunk-7ZYDC6ZN.mjs";import"./chunk-JV2FTZJZ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as app,b as default};
